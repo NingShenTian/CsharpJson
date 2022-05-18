d690375f30801cf2a746c44bc1593517aa1294ec
xiaoning <NingShenTian@users.noreply.github.com>
2022-05-18 16:31:20 +0800

    Update JsonValue.cs

--- CsharpJson/JsonValue.cs
+++ CsharpJson/JsonValue.cs
@@ -4,7 +4,7 @@
 //  Author:
 //       <springrain1991@hotmail.com>
 //
-//  Copyright (c) 2017 田小宁
+//  Copyright (c) 2017 tianxiaoning
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
